820c6e693e562729e512095e70e3e83d62152cb4
lyklecharova <lklecharova9@gmail.com>
2024-03-02 13:13:38 +0200

    Update home.component.ts

--- client/culinary-recipes/src/app/pages/home/home.component.ts
+++ client/culinary-recipes/src/app/pages/home/home.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(private title: Title) { }
 
+  ngOnInit(): void {
+    this.title.setTitle('Home');
+  }
 }
