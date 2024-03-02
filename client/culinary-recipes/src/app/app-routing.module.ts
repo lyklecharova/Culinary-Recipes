498d28940e15de78e2518c831e0aa9a92098a02e
lyklecharova <lklecharova9@gmail.com>
2024-03-02 13:12:50 +0200

    Update app-routing.module.ts

--- client/culinary-recipes/src/app/app-routing.module.ts
+++ client/culinary-recipes/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
