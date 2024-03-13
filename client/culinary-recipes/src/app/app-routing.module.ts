09c3bc033ca23b1cc43da8031cd626ad8916a70c
lyklecharova <lklecharova9@gmail.com>
2024-03-13 16:57:41 +0200

    Update app-routing.module.ts

--- client/culinary-recipes/src/app/app-routing.module.ts
+++ client/culinary-recipes/src/app/app-routing.module.ts
@@ -1,18 +1,76 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+
+import { DashboardComponent } from './recipes/dashboard/dashboard.component';
+import { CreateComponent } from './recipes/create/create.component';
+import { EditComponent } from './recipes/edit/edit.component';
 import { ErrorComponent } from './pages/error/error.component';
+import { RegisterComponent } from './pages/user/register/register.component';
+import { LoginComponent } from './pages/user/login/login.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { DetailsComponent } from './recipes/details/details.component';
+
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/home' },
-  { path: 'home', component: HomeComponent },
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    // canActivate: [AuthGuard],
+    // data: {
+    //   guest: true,
+    // },
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    // canActivate: [AuthGuard],
+    // data: {
+    //   guest: true,
+    // },
+  },
+  {
+    path: 'about-us',
+    component: AboutUsComponent,
+
+  },
+  {
+    path: 'contacts',
+    component: ContactsComponent,
+
+  },
+  {
+    path: 'recipes',
+    component: DashboardComponent,
+
+  },
+  {
+    path: 'create-recipe',
+    component: CreateComponent,
+
+  },
+  {
+    path: 'recipe/:id',
+    component: DetailsComponent,
+
+  },
+  {
+    path: 'edit-recipe',
+    component: EditComponent,
+
+  },
   { path: '**', redirectTo: '/404' },
   { path: '404', component: ErrorComponent },
- 
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
