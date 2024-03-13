e109223f0bec2e2de48c5f6de3b88a76c460f2f5
lyklecharova <lklecharova9@gmail.com>
2024-03-13 16:57:47 +0200

    Update app.module.ts

--- client/culinary-recipes/src/app/app.module.ts
+++ client/culinary-recipes/src/app/app.module.ts
@@ -8,15 +8,14 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 
-import { MainComponent } from './main/main.component';
 import { UserModule } from './pages/user/user.module';
 import { RecipesModule } from './recipes/recipes.module';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent,
   ],
   imports: [
     BrowserModule,
