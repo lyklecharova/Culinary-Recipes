d96277309e44a794b3060b7aa4746784f5b80931
lyklecharova <lklecharova9@gmail.com>
2024-03-13 16:58:45 +0200

    Update pages.module.ts

--- client/culinary-recipes/src/app/pages/pages.module.ts
+++ client/culinary-recipes/src/app/pages/pages.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { ErrorComponent } from './error/error.component';
-
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    ErrorComponent
+    ErrorComponent,
+    AboutUsComponent,
+    ContactsComponent
   ],
   imports: [CommonModule],
 })
