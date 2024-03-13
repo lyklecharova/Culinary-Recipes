83e45fce5b1a181f4a87ac378ed6f7e3603cc4ab
lyklecharova <lklecharova9@gmail.com>
2024-03-13 18:18:32 +0200

    Update api.service.ts

--- client/culinary-recipes/src/app/api.service.ts
+++ client/culinary-recipes/src/app/api.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
 import { environment } from 'src/environments/environment';
+import { Recipe } from './types/recipe';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +12,13 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   getRecipes(){
-    const api = environment.API_URL;	
+    const { API_URL } = environment;	
+    return this.http.get<Recipe[]>(`${API_URL}/recipe`);
+  }
+  
+  createRecipe(title: string) {
+    const { API_URL } = environment;
+    const payload = { title };
+    return this.http.post<Recipe>(`${API_URL}/recipe`, payload); // Use correct interpolation for API_URL
   }
 }
