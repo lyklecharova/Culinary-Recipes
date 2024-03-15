1dcf5e02a1a287211b3a144bd1c660a18281542f
lyklecharova <lklecharova9@gmail.com>
2024-03-15 10:29:57 +0200

    Update shared.module.ts

--- client/culinary-recipes/src/app/shared/shared.module.ts
+++ client/culinary-recipes/src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { EmailDirective } from './validators/email.directive';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    EmailDirective
+  ],
   imports: [
     CommonModule
   ]
