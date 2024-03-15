d99a6123a9129968e6217bdd2c8f81ca0a07d90c
lyklecharova <lklecharova9@gmail.com>
2024-03-15 10:19:31 +0200

    Update login.component.ts

--- client/culinary-recipes/src/app/pages/user/login/login.component.ts
+++ client/culinary-recipes/src/app/pages/user/login/login.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  constructor(private userService: UserService, private router: Router) {}
 
+  login(form: NgForm) {
+    console.log(form.value);
+    if (form.invalid) {
+      return;
+    }
+
+    this.userService.login();
+    this.router.navigate(['/home']);
+  }
 }
