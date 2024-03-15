466ee63494b796caf810322f92729cba3091e179
lyklecharova <lklecharova9@gmail.com>
2024-03-15 21:03:46 +0200

    Update user.service.ts

--- client/culinary-recipes/src/app/pages/user/user.service.ts
+++ client/culinary-recipes/src/app/pages/user/user.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { UserForAuth } from 'src/app/types/user';
+
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  user: UserForAuth | undefined;
+  USER_KEY = '[user]';
 
-  constructor() { }
+  get isLogged(): boolean {
+    return !!this.user;
+  }
+
+  constructor() {
+    try {
+      const lsUser = localStorage.getItem(this.USER_KEY) || '';
+      this.user = JSON.parse(lsUser);
+    } catch (error) {
+      this.user = undefined;
+    }
+  }
+
+  login() {
+    this.user = {
+      id: '5fa64ca72183ce1728ff3726',
+      email: 'petkoivanov@abv.bg',
+      password: '123123',
+    };
+
+    localStorage.setItem(this.USER_KEY, JSON.stringify(this.user));
+  }
+
+  logout() {
+    this.user = undefined;
+    localStorage.removeItem(this.USER_KEY);
+  }
 }
