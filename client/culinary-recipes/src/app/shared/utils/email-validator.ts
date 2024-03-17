785583b1bc4c4e016a98351faaf75a08c84bb11c
lyklecharova <lklecharova9@gmail.com>
2024-03-17 21:26:07 +0200

    Update email-validator.ts

--- client/culinary-recipes/src/app/shared/utils/email-validator.ts
+++ client/culinary-recipes/src/app/shared/utils/email-validator.ts
@@ -5,7 +5,6 @@ export function emailValidator(domains: string[]): ValidatorFn {
   const EMAIL = new RegExp(/^([^@\s]+)@(abv\.bg|gmail\.${domainString})$/);
   return (control) => {
     const isEmailInvalid = EMAIL.test(control.value);
-    console.log(control.value);
     if (!isEmailInvalid) {
       return { emailValidator: true };
     }
