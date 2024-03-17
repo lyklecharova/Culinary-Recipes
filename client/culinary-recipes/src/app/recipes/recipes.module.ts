b732174ad9f8e26cbbe2a455cce9a5e66d0870cb
lyklecharova <lklecharova9@gmail.com>
2024-03-17 18:13:28 +0200

    Update recipes.module.ts

--- client/culinary-recipes/src/app/recipes/recipes.module.ts
+++ client/culinary-recipes/src/app/recipes/recipes.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CreateComponent } from './create/create.component';
 import { DetailsComponent } from './details/details.component';
 import { EditComponent } from './edit/edit.component';
-import { RouterModule } from '@angular/router';
+import { DeleteComponent } from './delete/delete.component';
+
 import { SharedModule } from '../shared/shared.module';
-import { RecipesRoutingModule } from './recipes-routing.module';
+
 
 
 @NgModule({
@@ -15,13 +18,14 @@ import { RecipesRoutingModule } from './recipes-routing.module';
     DashboardComponent,
     CreateComponent,
     DetailsComponent,
-    EditComponent
+    EditComponent,
+    DeleteComponent,
   ],
   imports: [
     CommonModule,
     RouterModule,
     SharedModule,
-    RecipesRoutingModule
+    FormsModule
   ],
 })
 export class RecipesModule { }
