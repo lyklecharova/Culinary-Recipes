e1db3d32da1c06e12f0fe23a1cba5fb494040308
lyklecharova <lklecharova9@gmail.com>
2024-03-17 19:55:27 +0200

    Update email-validator.ts

--- client/culinary-recipes/src/app/shared/utils/email-validator.ts
+++ client/culinary-recipes/src/app/shared/utils/email-validator.ts
@@ -1,9 +1,14 @@
 import { ValidatorFn } from '@angular/forms';
 
 export function emailValidator(domains: string[]): ValidatorFn {
-  const EMAIL = new RegExp(`/^([^@\s]+)@(abv\.bg|domain\.com)$/`);
+  const domainString = domains.join('|');
+  const EMAIL = new RegExp(/^([^@\s]+)@(abv\.bg|gmail\.${domainString})$/);
   return (control) => {
-    const isEmailInvalid = control.value === '' || EMAIL.test(control.value);
-    return isEmailInvalid ? null : { emailValidator: true };
+    const isEmailInvalid = EMAIL.test(control.value);
+    console.log(control.value);
+    if (!isEmailInvalid) {
+      return { emailValidator: true };
+    }
+    return null;
   };
 }
