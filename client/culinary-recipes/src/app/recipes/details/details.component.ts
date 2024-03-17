9b77e97b6b91794118ebb90a4e3faf7f424af12d
lyklecharova <lklecharova9@gmail.com>
2024-03-17 18:37:29 +0200

    Update details.component.ts

--- client/culinary-recipes/src/app/recipes/details/details.component.ts
+++ client/culinary-recipes/src/app/recipes/details/details.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { ApiService } from 'src/app/api.service';
+import { Recipe } from 'src/app/types/recipe';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.css']
+  styleUrls: ['./details.component.css'],
 })
-export class DetailsComponent {
-
+export class DetailsComponent implements OnInit {
+  recipe: Recipe | undefined;
+  constructor(private api: ApiService, private route: ActivatedRoute) {}
+  ngOnInit(): void {
+    const recipeId = this.route.snapshot.params['id'];
+    this.api.getOneRecipe(recipeId).subscribe((recipe) => {
+      this.recipe = recipe;
+    });
+  }
 }
-
