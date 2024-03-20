4ac15080f161a1752e525232a72e0def61c7e2fc
lyklecharova <lklecharova9@gmail.com>
2024-03-20 18:45:55 +0200

    Update edit.component.ts

--- client/culinary-recipes/src/app/recipes/edit/edit.component.ts
+++ client/culinary-recipes/src/app/recipes/edit/edit.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
-  styleUrls: ['./edit.component.css']
+  styleUrls: ['./edit.component.css'],
 })
-export class EditComponent {
-
+export class EditComponent implements OnInit {
+  constructor(private api: ApiService, private route: ActivatedRoute) {}
+  currentId: string = '';
+  editRecipe(form: NgForm) {
+    if (form.valid) {
+      console.log(form.value);
+      this.api.editRecipe(form.value, this.currentId).subscribe((result) => {
+        // console.log(result);
+      });
+    }
+  }
+  ngOnInit(): void {
+    this.currentId = this.route.snapshot.params['id'];
+  }
 }
