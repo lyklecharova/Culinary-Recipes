596883ad828513962c664058d321d4b63a722caf
lyklecharova <lklecharova9@gmail.com>
2024-03-20 18:47:53 +0200

    Update pages.module.ts

--- client/culinary-recipes/src/app/pages/pages.module.ts
+++ client/culinary-recipes/src/app/pages/pages.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { HomeComponent } from './home/home.component';
 import { ErrorComponent } from './error/error.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactsComponent } from './contacts/contacts.component';
-
 
 @NgModule({
   declarations: [
     HomeComponent,
     ErrorComponent,
     AboutUsComponent,
-    ContactsComponent
+    ContactsComponent,
   ],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
 })
 export class PagesModule {}
