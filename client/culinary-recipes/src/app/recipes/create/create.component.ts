6bda97f4b3f219b29c2bf39dec85c40c7e8e7036
lyklecharova <lklecharova9@gmail.com>
2024-03-21 08:07:56 +0200

    Update create.component.ts

--- client/culinary-recipes/src/app/recipes/create/create.component.ts
+++ client/culinary-recipes/src/app/recipes/create/create.component.ts
@@ -3,6 +3,8 @@ import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { ApiService } from 'src/app/api.service';
+import { URL_PATTERN } from 'src/app/constants/constants';
+import { Recipe } from 'src/app/types/recipe';
 
 @Component({
   selector: 'app-create',
@@ -10,15 +12,64 @@ import { ApiService } from 'src/app/api.service';
   styleUrls: ['./create.component.css'],
 })
 export class CreateComponent {
+  imagePattern = URL_PATTERN;
+
   constructor(private api: ApiService, private router: Router) {}
 
-  addRecipe(form: NgForm) {
+  addRecipe(form: NgForm): boolean {
     if (form.valid) {
-      console.log(form.value);
-      this.api.createRecipe(form.value).subscribe((result) => {
-        // console.log(result);
+      // Кастване на form.value към тип 'Recipe'
+      const formValues = form.value as Recipe;
+
+      // Проверка за празни полета
+      const isAnyFieldEmpty = Object.values(formValues).some((value: any) => {
+        if (typeof value === 'string') {
+          return value.trim() === '';
+        }
+        return false;
       });
+
+      if (isAnyFieldEmpty) {
+        // Показва грешка, ако има празни полета
+        console.log('Please fill in all fields.');
+        return false;
+      }
+
+      // Проверка за невалидни данни в description и ingredients
+      if (
+        typeof formValues.description === 'string' &&
+        formValues.description.trim().length < 10
+      ) {
+        console.log('Description must be at least 10 characters long.');
+        return false;
+      }
+
+      // Съединява елементите на масива ingredients в един низ с интервал между тях
+      const ingredientsString = Array.isArray(formValues.ingredients)
+        ? formValues.ingredients.join(' ')
+        : formValues.ingredients;
+
+      if (
+        typeof ingredientsString === 'string' &&
+        ingredientsString.trim().length < 10
+      ) {
+        console.log('Ingredients must be at least 10 characters long.');
+        return false;
+      }
+
+      // Изпращане на данните до API
+      this.api.createRecipe(form.value).subscribe((result) => {
+        console.log('Recipe created successfully:', result);
+        // Редирект след успешно създаване на рецепта
+        this.router.navigate(['/recipes']);
+      });
+
+      // Връщаме true, за да покажем, че процесът е успешен
+      return true;
+    } else {
+      console.log('Please fill in all required fields.');
+      // Връщаме false, за да предотвратим навигацията към следващата страница
+      return false;
     }
-    this.router.navigate(['/recipes'])
   }
 }
