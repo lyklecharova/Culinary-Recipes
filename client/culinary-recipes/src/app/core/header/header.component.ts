c5f889432e1c75aeb62c1c25761435a3a8327963
lyklecharova <lklecharova9@gmail.com>
2024-03-24 16:35:27 +0200

    Update header.component.ts

--- client/culinary-recipes/src/app/core/header/header.component.ts
+++ client/culinary-recipes/src/app/core/header/header.component.ts
@@ -8,15 +8,16 @@ import { UserService } from 'src/app/pages/user/user.service';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-
   constructor(private user: UserService, private router: Router) {}
-  isLogged: boolean = true;
-  // get isLogged(): boolean {
-  //   return this.user.isLogged
-  // }
-  
+  // isLogged: boolean = true;
+  get isLogged(): boolean {
+    console.log(this.user.isLogged, ' RETUNR LOGIN INFOOOOO');
+    return this.user.isLogged
+  }
 
   logout() {
     this.user.logout();
+    localStorage.removeItem('userId');
+    localStorage.removeItem('token');
   }
 }
