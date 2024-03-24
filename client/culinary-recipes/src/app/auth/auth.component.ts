f69a44ea387adb9518dc92b44bef922fd8c8cc84
lyklecharova <lklecharova9@gmail.com>
2024-03-24 10:42:56 +0200

    Create auth.component.ts

---
+++ client/culinary-recipes/src/app/auth/auth.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../pages/user/user.service';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css'],
+})
+export class AuthComponent {}
