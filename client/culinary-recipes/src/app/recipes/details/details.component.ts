39e83b12bca48d33b464d30820b8708aa7616eae
lyklecharova <lklecharova9@gmail.com>
2024-03-26 13:26:10 +0200

    Update details.component.ts

--- client/culinary-recipes/src/app/recipes/details/details.component.ts
+++ client/culinary-recipes/src/app/recipes/details/details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute} from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { ApiService } from 'src/app/api.service';
 import { Recipe } from 'src/app/types/recipe';
@@ -10,15 +10,35 @@ import { Recipe } from 'src/app/types/recipe';
   styleUrls: ['./details.component.css'],
 })
 export class DetailsComponent implements OnInit {
+  userId = localStorage.getItem('userId');
+  recipeOwnerId = '';
   recipe: Recipe | undefined;
-  currentRecipeId?: string = '';
-  constructor(private api: ApiService, private route: ActivatedRoute) {}
+  currentRecipeId: string = '';
+  constructor(
+    private api: ApiService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
   ngOnInit(): void {
     const recipeId = this.route.snapshot.params['id'];
     this.api.getOneRecipe(recipeId).subscribe((recipe) => {
       this.recipe = recipe;
-      this.currentRecipeId = recipe._id;
+      this.currentRecipeId = recipe._id as unknown as string;
+      this.recipeOwnerId = recipe.ownerId as unknown as string;
     });
   }
-  
+
+  deleteRecipe(): void {
+    this.api.deleteRecipe(this.currentRecipeId).subscribe(
+      () => {
+        console.log('Recipe deleted successfully');
+        // Optionally, you can redirect to a different page after deletion
+        this.router.navigate(['/recipes']);
+      },
+      (error) => {
+        console.error('Error deleting recipe:', error);
+        // Handle error
+      }
+    );
+  }
 }
