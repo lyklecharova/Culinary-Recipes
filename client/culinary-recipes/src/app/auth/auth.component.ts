a5cb9a73b6d3aa3633ef7e4e882a8f795bd08f7e
lyklecharova <lklecharova9@gmail.com>
2024-03-27 08:33:40 +0200

    Delete auth.component.ts

--- client/culinary-recipes/src/app/auth/auth.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService } from '../pages/user/user.service';
-
-@Component({
-  selector: 'app-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css'],
-})
-export class AuthComponent {}
