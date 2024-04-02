9d25001d2a0c206419cddecedd2aaca0f071aa67
lyklecharova <lklecharova9@gmail.com>
2024-04-02 09:18:52 +0300

    Update details.component.ts

--- client/culinary-recipes/src/app/recipes/details/details.component.ts
+++ client/culinary-recipes/src/app/recipes/details/details.component.ts
@@ -11,20 +11,37 @@ import { Recipe } from 'src/app/types/recipe';
 })
 export class DetailsComponent implements OnInit {
   userId = localStorage.getItem('userId');
-  recipeOwnerId = '';
+  // recipeOwnerId = '';
+  recipeOwnerId: string | undefined;
+  isOwner: boolean = false;
   recipe: Recipe | undefined;
   currentRecipeId: string = '';
+
   constructor(
     private api: ApiService,
     private route: ActivatedRoute,
     private router: Router
   ) {}
+
   ngOnInit(): void {
     const recipeId = this.route.snapshot.params['id'];
     this.api.getOneRecipe(recipeId).subscribe((recipe) => {
       this.recipe = recipe;
+
       this.currentRecipeId = recipe._id as unknown as string;
       this.recipeOwnerId = recipe.ownerId as unknown as string;
+    
+      // Проверка и преобразуване на инструкциите и съставките
+      if (typeof this.recipe.ingredients === 'string') {
+        this.recipe.ingredients = (this.recipe.ingredients as string).split(
+          '\n'
+        );
+      }
+      if (typeof this.recipe.instructions === 'string') {
+        this.recipe.instructions = (this.recipe.instructions as string).split(
+          '\n'
+        );
+      }
     });
   }
 
@@ -40,5 +57,17 @@ export class DetailsComponent implements OnInit {
         // Handle error
       }
     );
+  }
+
+  confirmDelete(): void {
+    // Проверка дали текущият потребител е собственик на рецептата
+    if (this.userId === this.recipeOwnerId) {
+      const confirmed = confirm('Are you sure you want to delete this recipe?');
+      if (confirmed) {
+        this.deleteRecipe();
+      }
+    } else {
+      alert('You are not authorized to delete this recipe.');
+    }
   }
 }
