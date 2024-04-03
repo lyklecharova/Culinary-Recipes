2faecc092291a9a7417de92607601de90f5280c6
lyklecharova <lklecharova9@gmail.com>
2024-04-03 08:24:59 +0300

    Update api.service.ts

--- client/culinary-recipes/src/app/api.service.ts
+++ client/culinary-recipes/src/app/api.service.ts
@@ -28,7 +28,7 @@ export class ApiService {
 
   getRecipesByUser(userId: string): Observable<Recipe[]> {
     const { API_URL } = environment;
-    return this.http.get<Recipe[]>(`${API_URL}/recipe`).pipe(
+    return this.http.get<Recipe[]>(`${API_URL}/recipe/`).pipe(
       map((recipes) =>
         recipes.filter((recipe) => {
           return recipe.ownerId === userId;
@@ -36,7 +36,6 @@ export class ApiService {
       )
     );
   }
-
   editRecipe(recipe: Recipe, id: string) {
     const { API_URL } = environment;
     // Създава payload обект, който съдържа данните за рецептата
